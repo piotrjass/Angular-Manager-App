0f4f4c00938f2bb556a11b08ff4ac26166151efb
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-02-26 00:01:40 +0100

    Implementing drag-and-drop

--- src/core/components/user-dashboard/dashboard-space/dashboard-space.component.ts
+++ src/core/components/user-dashboard/dashboard-space/dashboard-space.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dashboard-space',
   standalone: true,
-  imports: [],
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './dashboard-space.component.html',
-  styleUrl: './dashboard-space.component.css'
+  styleUrl: './dashboard-space.component.css',
 })
 export class DashboardSpaceComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
---
+++ src/core/components/user-dashboard/drag-drop-item/drag-drop-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-drop-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './drag-drop-item.component.html',
+  styleUrl: './drag-drop-item.component.css'
+})
+export class DragDropItemComponent {
+
+}
