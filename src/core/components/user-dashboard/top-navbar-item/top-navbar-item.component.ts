f4b93f4554e0524e335a45b0f13741ec4ee5faf7
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-02-29 20:20:51 +0100

    Minor fix

--- src/core/components/user-dashboard/top-navbar-item/top-navbar-item.component.ts
+++ src/core/components/user-dashboard/top-navbar-item/top-navbar-item.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-top-navbar-item',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, RouterLink],
   templateUrl: './top-navbar-item.component.html',
   styleUrl: './top-navbar-item.component.css',
 })
 export class TopNavbarItemComponent {
   @Input() title: string = '';
+  @Input() icon: string = '';
+  @Input() link: string = '';
 }
--- src/core/components/user-dashboard/top-navbar/top-navbar.component.ts
+++ src/core/components/user-dashboard/top-navbar/top-navbar.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { TopNavbarItemComponent } from '../top-navbar-item/top-navbar-item.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
-  imports: [TopNavbarItemComponent],
+  imports: [TopNavbarItemComponent, RouterLink],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.css',
 })
 export class TopNavbarComponent {
-  titles: string[] = ['Calendar', 'Ideas', 'Goals'];
+  components: any[] = [
+    {
+      title: 'Calendar',
+      icon: 'calendar_month',
+      link: 'calendar',
+    },
+    { title: 'Ideas', icon: 'lightbulb', link: 'ideas' },
+    { title: 'LongTerm', icon: 'rocket_launch', link: 'LongTerm' },
+  ];
 }
