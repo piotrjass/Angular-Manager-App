47dc445785987444f6751851ba66f3d395402929
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-03-11 23:45:27 +0100

    Fixed top navbar

--- src/core/components/user-dashboard/top-navbar/top-navbar.component.ts
+++ src/core/components/user-dashboard/top-navbar/top-navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TopNavbarItemComponent } from '../top-navbar-item/top-navbar-item.component';
 import { RouterLink } from '@angular/router';
-
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
-  imports: [TopNavbarItemComponent, RouterLink],
+  imports: [TopNavbarItemComponent, RouterLink, MatIcon, CommonModule],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.css',
 })
 export class TopNavbarComponent {
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
   components: any[] = [
     {
       title: 'Calendar',
@@ -19,4 +25,15 @@ export class TopNavbarComponent {
     { title: 'Ideas', icon: 'lightbulb', link: 'ideas' },
     { title: 'LongTerm', icon: 'rocket_launch', link: 'long-term' },
   ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    if (window.innerWidth > 768) {
+      this.isMenuOpen = true;
+    }
+  }
 }
