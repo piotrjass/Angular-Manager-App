2518cf0c011e9e3c7b157df58a1ac6fc3b2080c7
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-03-12 00:13:14 +0100

    Added progress bar

--- src/core/components/user-dashboard/side-navbar-card/side-navbar-card.component.ts
+++ src/core/components/user-dashboard/side-navbar-card/side-navbar-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ModuleWithProviders } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 @Component({
   selector: 'app-side-navbar-card',
   standalone: true,
-  imports: [],
+  imports: [NgCircleProgressModule],
   templateUrl: './side-navbar-card.component.html',
   styleUrl: './side-navbar-card.component.css',
+  providers: [
+    (
+      NgCircleProgressModule.forRoot({
+        radius: 100,
+        outerStrokeWidth: 16,
+        innerStrokeWidth: 8,
+        outerStrokeColor: '#78C000',
+        innerStrokeColor: '#C7E596',
+        animationDuration: 300,
+      }) as ModuleWithProviders<NgCircleProgressModule>
+    ).providers!,
+  ],
 })
 export class SideNavbarCardComponent {}
